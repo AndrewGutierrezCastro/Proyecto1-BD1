5161c1d7d5b2dbdd928c77940605cbbf4f340cd3
AndrewGutierrezCastro <andrewgc@estudiantec.cr>
2020-06-02 12:52:04 -0600

    Clientes funcionando completamente

--- Proyecto1-BD1/View/MenuGeneral.cs
+++ Proyecto1-BD1/View/MenuGeneral.cs
@@ -89,6 +89,7 @@ namespace Proyecto1_BD1
 
         private void listarClientesBtn_Click(object sender, EventArgs e)
         {
+            this.clientesTabControl.Show();
             //Generar Grid
             { listaOrganizacionesGrid = new DataGridView();
             listaPersonasGrid = new DataGridView();
@@ -124,7 +125,7 @@ namespace Proyecto1_BD1
         }
         private void insertarClienteBtn_Click(object sender, EventArgs e)
         {
-            
+            this.clientesTabControl.Show();
             this.crearPersona = new CrearPersona();
             this.crearPersona.Name = "crearPersona1";
             this.crearPersona.Size = new Size(374, 316);
@@ -263,6 +264,7 @@ namespace Proyecto1_BD1
         }
         private void modificarClienteBtn_Click(object sender, EventArgs e)
         {
+            this.clientesTabControl.Show();
             this.updatePersona = new UpdatePersona();
             this.updatePersona.Name = "updatePersona1";
             this.updatePersona.Size = new Size(379, 349);
